2dcf17990e9a4f4b1fd233df9eb5d5c559a80bb8
pabhoz <pabhoz@gmail.com>
2019-09-18 20:59:04 -0500

    Revisar notion para la tarea

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-var tracks = []
-var audio;
-var audioPreload;
-var preloaded = false;
+let tracks = []
+let player = document.querySelector('#player');
 
-var player = document.querySelector('#player');
-var audio = document.querySelector('#player audio');
-var trackElements = document.getElementsByClassName('track');
+let trackElements = document.getElementsByClassName('track');
 for (let i = 0; i < trackElements.length; i++) {
-    trackElements[i].addEventListener('click', function (e) {
-        playTrack(e);
-    }, false);
     tracks.push(trackElements[i].href);
 }
 
-audio.src = tracks[0];
-document.querySelector('#playpause').onclick = () => { play(); };
-function play(){
-    console.log('playing',audio);
-    audio.play();
-}
-
 var menu = document.querySelector('.menu');
-menu.onclick = () => { player.classList.toggle('show')};
+menu.onclick = () => { player.classList.toggle('show') };
+
+let myAudioPlayer = new MultimediaPlayer('#player audio', tracks, {
+    play: document.querySelector('#playpause'),
+    next: document.querySelector('#next'),
+    prev: document.querySelector('#back'),
+    title: document.querySelector('#title'),
+    artist: document.querySelector('#artist'),
+    album: document.querySelector('#album'),
+    cover: document.querySelector('#main'),
+    playlistMenu: document.querySelector('#playlist'),
+    progressBar: document.querySelector('#main .slider'),
+});
+
+console.log(myAudioPlayer);
